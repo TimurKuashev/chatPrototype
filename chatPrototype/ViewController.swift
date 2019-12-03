494169ed018884a94cdfdc903ab94b585d5f6d72
timur <timur@letmecode.org>
2019-12-03 10:16:09 +0300

    Initial Commit

---
+++ chatPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatPrototype
+//
+//  Created by Timur Kuashev on 03.12.2019.
+//  Copyright © 2019 Timur Kuashev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
